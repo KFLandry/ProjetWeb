f2703ff1007d07dedfadaa9ff56b539f19bb07b0
landry kankeu <118926620+KFLandry@users.noreply.github.com>
2024-03-10 11:22:40 +0100

    ...

--- index.php
+++ index.php
@@ -35,11 +35,11 @@ use Controller\DonationController;
 // http_response_code(204);
 
 // //On vérifie si on reçoit un token
+$ressource ="";
 if (isset($_SERVER['PATH_INFO'])){
     $uri = explode("/",$_SERVER['PATH_INFO']);
     $ressource  = $uri[1];
 }
-$ressource ="";
 //Token d'autorisation est indispensable pour toute les requêtes sauf celle d'authentification et de recuperation
 
 if ($ressource === 'signup' or $ressource === 'login' or $_SERVER['REQUEST_METHOD'] === "GET" or isset($_REQUEST)) {
