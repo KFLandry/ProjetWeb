6ab4a0417ed298ff4c90d25d50810acd839104a7
wilfrieddev0 <118926620+KFLandry@users.noreply.github.com>
2024-04-29 20:07:33 +0200

    That's right

--- src/controller/AbstractController.php
+++ src/controller/AbstractController.php
@@ -18,8 +18,7 @@ abstract class AbstractController{
         // Je force le verbe a PATCH pour une mediaUpdate
         $this->method = $this->ressource !=="mediaUpdate" ? $_SERVER["REQUEST_METHOD"] : "PATCH" ;
         $this->body = file_get_contents("php://input") ? json_decode(file_get_contents("php://input"),true) : $_REQUEST;
-        $this->id =   $_REQUEST['id'];
-        $this->id =$this->id ??  count($uri)> 2 ? $uri[2] : 0;//L'id =  identifiant compte || email compte
+        $this->id = isset($_REQUEST['id']) ? ($_REQUEST['id']) : ((count($uri))> 2 ? $uri[2] : 0);//L'id =  identifiant compte || email compte
     }
     public abstract function handleRequest ();
 
